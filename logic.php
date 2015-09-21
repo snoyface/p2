d9cac436b0d36648eff2fce7b6c99e654a0c218a
Jon MacLeod <j.macleod4@gmail.com>
2015-09-21 14:37:58 -0400

    update to logic.php

--- logic.php
+++ logic.php
@@ -7,10 +7,26 @@
 	<?php
 		//adding the words to the array
 		$passwordNum = array();
+
+		$numberOfWords  = $_GET["numwords"];
+		$includeNumbers = $_GET["numinclude"];
+		$includeChars   = $_GET["charinclude"];
 		
-		foreach ($_GET as $key => $value){
-			$rWord = rand(0,8);
-			$passwordNum[$value] = $words[$rWord];
+		for($i = 0; $i <= $numberOfWords; $i++){
+			//echo out all of the keys and values in $_GET
+			echo $key . '==' . $value . '<br>';
+			//generate a random number
+			$rWord = rand(0,9);
+			//echo out random num to be sure
+			echo 'Random num: ' . $rWord;
+			//push word in # position to array.
+			array_push($passwordNum, $words[$rWord]);
+/*
+			if(isset($passwordNum)){
+				array_push($passwordNum, $words[$rWord]);
+			}else{ $passwordNum[0] = $words[$rWord];
+				}
+*/
 		}
 	?>
 
