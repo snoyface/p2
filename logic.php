13b535aad66701c191694cb24d64c80d18627771
Jon MacLeod <j.macleod4@gmail.com>
2015-09-21 17:08:47 -0400

    num and char additions, exit if more than 9 words

--- logic.php
+++ logic.php
@@ -1,6 +1,9 @@
 <?php
 	//lets create a simple word array
-	$words = array('house','farm','battery','box','member','method','apple','secret','scarf','people');
+	$words   = array('house','farm','battery','box','member','method','apple','secret','scarf','people');
+	//lets create number and char arrays
+	$numbers = array('1','2','3','4','5','6','7','8','9','0');
+	$chars   = array('!','@','#','$','%','^','&','*','?','(',')','_','+');
 	?>
 
 	
@@ -12,6 +15,10 @@
 		$numberOfWords  = $_GET["numwords"]-1;
 		$includeNumbers = $_GET["numinclude"];
 		$includeChars   = $_GET["charinclude"];
+
+		if($numberOfWords >= 9 ){
+			exit ("Try again, too many words");
+		}
 		
 		for($i = 0; $i <= $numberOfWords; $i++){
 			//get a random number
@@ -19,6 +26,15 @@
 			
 			//push word in # position to array.
 			array_push($passwordNum, $words[$rWord]);
+		}
+
+		if($includeChars == 'Yes'){
+			$rWord = rand(0,9);
+			array_push($passwordNum, $chars[$rWord]);
+		}
+		if($includeNumbers == 'Yes'){
+			$rWord = rand(0,12);
+			array_push($passwordNum, $numbers[$rWord]);
 		}
 	?>
 
